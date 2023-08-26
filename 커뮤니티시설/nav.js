b37b08733971e67598a2500f4408af8accf1939a
ttfcl <ktx1016@naver.com>
2023-08-26 14:32:52 +0900

    run12312321231133

--- 커뮤니티시설/nav.js
+++
@@ -1,9 +0,0 @@
-let navlogo = document.querySelector("#logo")
-
-window.addEventListener('scroll', function(){
-    if(window.scrollY > 100) {
-        navlogo.classList.add("hide")
-    }else if(window.scrollY <= 100){
-        navlogo.classList.remove("hide")
-    }
-});
